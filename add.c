95252ba6ac987b6692ce402eb43421a2c948cf17
lanying0218 <26840715@qq.com>
2017-07-06 13:51:01 +0800

    Update add.c

--- add.c
+++ add.c
@@ -1,10 +1,17 @@
 /*比较两个数的大小*/
-#include <stdio.h>
+include <stdio.h>
+
 int main()
+
 {
     int a,b,max;
-    scanf("请输入两个整数：%d%d",&a,&b);
-    max={a>b?a:b}
-    printf("大数是：%d\n",max);
+
+        scanf("%d%d",&a,&b);
+
+              max=a>b?a:b;
+
+        printf("%d\n",max);
+    
     return 0;
+
 }
