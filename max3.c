75a273bbd0d6c3263b9ed8076957a84b1beb187d
lanying0218 <26840715@qq.com>
2017-07-08 09:01:08 +0800

    Create max3.c
    比较三个数的大小，输出最大数。

---
+++ max3.c
@@ -0,0 +1,22 @@
+/*题目：比较三个数的大小，并输出最大数。
+ *作者：蓝鹰
+ *时间：2017-07-08
+*/
+
+#include <stdio.h>
+
+int max3(int x,int y,int z)
+{
+        int u;
+        u = x>y?x:y;
+        u = u>z?u:z;
+        return (u);
+}
+
+int main()
+{
+        int a,b,c,d;
+        scanf("%d%d%d",&a,&b,&c);
+        d=max3(a,b,c);
+        printf("max=%d\n",d);
+}
